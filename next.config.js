648721770ec81910973bb55f4a95d850eac4ceac
damie824 <booboo4055@gmail.com>
2023-12-17 22:56:25 +0900

    폰트 수정 4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizeFonts: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
